451064d7b13e093fb38c08eef9ddc84e3bbc7eeb
Samantha Ramos <samantharamos@samanthas-air.attlocal.net>
2017-09-25 12:16:30 -0500

    fucking heroku

--- public/app.js
+++ public/app.js
@@ -5,8 +5,7 @@ app.controller('spController', ['$http', '$scope', function($http, $scope) {
   $scope.modalShown = false;
   const controller = this;
   this.message = "controller works";
-  this.url = 'https://smartypantsbackend.herokuapp.com';
-    // 'http://localhost:3000'
+  this.url = 'https://smartypantsbackend.herokuapp.com' || 'http://localhost:3000';
   this.post = {};
   this.formData = {};
   // this.testArray = [1, 2, 3];
